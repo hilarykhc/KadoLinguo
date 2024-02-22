dd49a947311e3fadde0e8dea03fa15bf310776a9
Hilary Chen <hilarychen@Hilarys-MacBook-Pro.local>
2024-02-22 05:44:09 -0500

    update homepage, about section

--- src/components/AboutOli/AboutOli.js
+++ src/components/AboutOli/AboutOli.js
@@ -7,19 +7,32 @@ const titleStyles = {
 export default function AboutOli() {
   return (
     <section className="homepage__about">
-      <h2 className="homepage__about-subtitle" style={titleStyles}>
-        Who is Oli?
-      </h2>
-      <p className="homepage__about-content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu pharetra
-        orci. Integer id tellus vel mauris ornare vulputate. Curabitur auctor
-        varius pellentesque. Suspendisse nec tellus massa. Donec vel felis elit.
-        Praesent et mi justo. Donec gravida, nibh ac tincidunt iaculis, mi
-        ligula faucibus enim, eget congue velit quam vitae felis. Sed dignissim
-        eleifend sollicitudin. Maecenas euismod eu ligula ornare imperdiet.
-        Phasellus risus lacus, dictum id augue at, pulvinar euismod massa. Nunc
-        et nibh libero.
-      </p>
+      <div className="homepage__about-container">
+        <div className="homepage__about-kadolinguo">
+          <h2 className="homepage__about-subtitle" style={titleStyles}>
+            What is KādoLinguo?
+          </h2>
+          <p className="homepage__about-content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu
+            pharetra orci. Integer id tellus vel mauris ornare vulputate.
+            Curabitur auctor varius pellentesque. Suspendisse nec tellus massa.
+            Donec vel felis elit. Praesent et mi justo. Donec gravida, nibh ac
+            tincidunt iaculis, mi ligula faucibus enim, eget congue velit quam.
+          </p>
+        </div>
+        <div className="homepage__about-oli">
+          <h2 className="homepage__about-subtitle" style={titleStyles}>
+            Who is Oli?
+          </h2>
+          <p className="homepage__about-content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu
+            pharetra orci. Integer id tellus vel mauris ornare vulputate.
+            Curabitur auctor varius pellentesque. Suspendisse nec tellus massa.
+            Donec vel felis elit. Praesent et mi justo. Donec gravida, nibh ac
+            tincidunt iaculis, mi ligula faucibus enim, eget congue velit quam.
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
