9540ec30451237b5387b4efc7c1f089466af6733
hilarykhc <hilary.kh.chen@gmail.com>
2024-03-13 05:02:40 -0400

    added BooksPage js and scss

--- src/pages/BooksPage/BooksPage.js
+++ src/pages/BooksPage/BooksPage.js
@@ -1,34 +1,7 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./BooksPage.scss";
+
 export default function BooksPage() {
-  return (
-    <main>
-      {/* <section className="story__list">
-        <div className="story__list-wrapper">
-          {stories.map((story) => (
-            <div
-              key={story.id}
-              className={`story__list-wrapper-card ${
-                currentImgId === story.id ? "clicked" : ""
-              }`}
-              onClick={() => {
-                handleSelectStory(story.id);
-                handleImgClick(story.id);
-              }}
-            >
-              <article className="story__list-wrapper-card2">
-                <div>
-                  <img
-                    src={story.image}
-                    alt={story.title}
-                    className={`story__list-wrapper-card-img ${
-                      currentImgId === story.id ? "default-grayscale" : ""
-                    }`}
-                  />
-                </div>
-              </article>
-            </div>
-          ))}
-        </div>
-      </section> */}
-    </main>
-  );
+  return <main></main>;
 }
